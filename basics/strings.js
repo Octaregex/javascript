292d39f4ea15d3f0075255d2625525d39e2d4a03
sachin sain <113036645+Octaregex@users.noreply.github.com>
2024-08-06 17:59:34 +0000

    strings in javascript

---
+++ basics/strings.js
@@ -0,0 +1,24 @@
+// first method to declare
+// let emailAddress = "sachinsain953@gmail.com"
+// emailAddress = "sunny@gmail.com"
+// console.log(emailAddress)
+
+// // second method to declare string
+// let url = new String("https://sachin-sain-vlogs")
+// templete literals or string literals (multi-line strings declare and concatination is easy)
+let str = `sachinsain953@gmail.com`
+// console.log(`my name is ${str} sain`)
+console.log(str.length)
+console.log(str.charAt(2))
+console.log(str.indexOf('i'))
+console.log(str.toUpperCase())
+console.log(str.toLowerCase())
+console.log(str.split('i'))
+console.log(str.replace('sain','dhurve'))
+console.log(str.substring(2,8))
+console.log(str.charCodeAt(5))
+console.log(str.concat(' ','sain'))
+console.log(str.includes('sain'))
+console.log(str.slice(5,10))
+console.log(str.search('sain'))// return the index if found else return -1
+console.log(str.trim(5)) // remove whitespace from both ends thus there is no whitespace so no trim needed.
