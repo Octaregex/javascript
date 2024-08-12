a9ed10e302147c4590597269401035ebf827fb03
sachin sain <113036645+Octaregex@users.noreply.github.com>
2024-08-12 09:31:57 +0000

    array ka funda

---
+++ basics2.0/array.js
@@ -0,0 +1,48 @@
+// let myArr = ["sachin","sunny","yash"]
+// let myArr = new Array(1,2,3,4)
+// console.log(myArr)
+// console.log(myArr)
+// myArr.push(4,3)
+// console.log(myArr)
+// myArr.pop()
+// console.log(myArr)
+// myArr.unshift("srijan")
+// console.log(myArr)
+// myArr.shift()
+// console.log(myArr)
+
+// merging methods of array 
+
+const myArr1 = [1,2,3,4]
+const myArr2 = [5,6,7,8]
+const myArr3 = [9,10,11,12]
+//myArr1.push(myArr2)
+// myArr2.push(myArr3)
+// console.log(myArr1[4][4][3]) // merge array within array
+// const array4 = myArr1.concat(myArr2)
+// console.log(array4) // merge array by forming a new sinle array
+
+// spread operator
+
+// const array4 = [...myArr1, ...myArr2, ...myArr3]
+// console.log(array4)
+
+// // flat method
+// const array5 = [1,2,3,4,[3,4,5,6,[4,5,6,78]]]
+// console.log(array5.flat(Infinity))
+
+// array or not 
+// console.log(Array.isArray("sachin"))
+// // convert to an array
+// console.log(Array.from("sachin"))
+// console.log(Array.from({name: "sachin"})) // important
+
+
+// const score1 = [1,2,3]
+// const score2 = [4,5,6]
+// const score3 = [7,8,9]
+// console.log(Array.of(score1,score2,score3))
+
+// console.log(Array.isArray(new Array("sachin")))
+
+
