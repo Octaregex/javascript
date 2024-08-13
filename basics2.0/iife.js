483dc599012575bc00f02acf6d51acd3ed84b401
sachin sain <113036645+Octaregex@users.noreply.github.com>
2024-08-13 10:58:27 +0000

    IIFE

---
+++ basics2.0/iife.js
@@ -0,0 +1,10 @@
+// immediate invoked function expression(IIFE)
+
+(function chole(){
+    // named iife
+    console.log(`sachin here`)
+})();
+
+((name) => {
+    console.log(`${name}, welcome to database`)
+})("sachin")
